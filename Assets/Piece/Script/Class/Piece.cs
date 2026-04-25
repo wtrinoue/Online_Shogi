32a9ad3729ed5cb4326da7ec685cec2f211c1938
wtr <w.inou.24.8.1574@4nonome.com>
2026-04-25 09:49:17 +0900

    fix piece data
    PieceDataに先手か後手かとどのような駒の種類かを設定するenumを付け加えた。

--- Assets/Piece/Script/Class/Piece.cs
+++ Assets/Piece/Script/Class/Piece.cs
@@ -1,13 +1,7 @@
 using UnityEngine;
-public enum Team
-{
-    Sente,
-    Gote
-}
 public class Piece
 {
     public PieceData data;
-    public Team team;
     public bool isPromoted;
 
     public MovePattern GetMove()
--- Assets/Piece/Script/ScriptableObject/PieceData.cs
+++ Assets/Piece/Script/ScriptableObject/PieceData.cs
@@ -2,8 +2,28 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Asset/PieceData")]
 public class PieceData : ScriptableObject
 {
+    public Team team;
+    public PieceType type;
     public Sprite normalSprite;
     public Sprite promotedSprite;
     public MovePattern normalMove;
     public MovePattern promotedMove;
+}
+
+public enum Team
+{
+    Sente,
+    Gote
+}
+
+public enum PieceType
+{
+    Fu,      // 歩
+    Kyo,     // 香
+    Kei,     // 桂
+    Gin,     // 銀
+    Kin,     // 金
+    Kaku,    // 角
+    Hisha,   // 飛
+    Ou       // 王（またはGyokuでもOK）
 }
