6fa5eb0d6a0b4ef4cf29719a6609c9f140392df9
wtr <w.inou.24.8.1574@4nonome.com>
2026-05-01 19:24:55 +0900

    add fucntion to show result to EndState
    EndStateでStateModuleを用いてリザルトパネルを表示できるようにした。

--- Assets/StateMachine/Script/State/EndState.cs
+++ Assets/StateMachine/Script/State/EndState.cs
@@ -12,10 +12,12 @@ public class EndState : State
     {
         Debug.Log($"EndStateに入りました。勝者: {winner}");
         StateModule.Viewer.BuildAll();
+        StateModule.Result.Show($"{winner}の勝ち！");
     }
 
     public override void Exit()
     {
+        StateModule.Result.Hide();
     }
 
     public override void OnClick(Vector2 pos)
