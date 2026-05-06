34d747d88aa0115e40f2431cf26e5d6fd1721bd7
wtr <w.inou.24.8.1574@4nonome.com>
2026-05-06 19:10:53 +0900

    fix NetworkWaitState login
    待っているときのコルーチンのWhileの条件がおかしかったので、修正をした。

--- Assets/StateMachine/Script/State/NetworkWaitState.cs
+++ Assets/StateMachine/Script/State/NetworkWaitState.cs
@@ -10,25 +10,28 @@ public class NetworkWaitState : State
     public override void Enter()
     {
         Debug.Log("NetworkWaitStateに入りました。相手の手を待っています…");
+        context.text.Show("相手が手を打っています");
         context.manager.ChangeIsMovedTo(false);
         context.machine.RunCoroutine(WaitForMoveCoroutine());
     }
 
     private IEnumerator WaitForMoveCoroutine()
     {
-        while (context.manager.GetIsMoved())
+        while (!context.manager.GetIsMoved())
         {
-            Debug.Log("一生はまっています");
+            Debug.Log("相手の手を待機中...");
             context.manager.ClearCells();
             context.viewer.BuildAll();
-            yield return  new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.5f);
         }
 
+        Debug.Log("相手の手を検知しました");
         context.machine.ChangeState(new NetworkJudgeState(context));
     }
 
     public override void Exit()
     {
+        context.text.Hide();
     }
 
     public override void OnClick(Vector2 pos)
